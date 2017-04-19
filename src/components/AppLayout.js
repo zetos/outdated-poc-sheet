598e0ebea1f05a236da36bf4d8f96adee2c4b76c
Zetos Lima <cesar_manoel@hotmail.com>
2017-04-19 00:41:25 -0300

    AppLayout component

---
+++ src/components/AppLayout.js
@@ -0,0 +1,38 @@
+import React, {PropTypes} from 'react';
+import {AppBar, Checkbox, IconButton, Layout, NavDrawer, Panel, Sidebar} from 'react-toolbox';
+
+const AppLayout = ({drawerActive, drawerPinned, sidebarPinned, toggleUiItem}) => {
+    <Layout>
+        <NavDrawer active={drawerActive}
+            pinned={drawerPinned} permanentAt='xxxl'
+            onOverlayClick={toggleDrawerActive}>
+            <p>
+                Navigation, account switcher, etc. go here.
+                    </p>
+        </NavDrawer>
+        <Panel>
+            <AppBar leftIcon='menu' onLeftIconClick={toggleUiItem('DrawerActive')} />
+            <div style={{flex: 1, overflowY: 'auto', padding: '1.8rem'}}>
+                <h1>Main Content</h1>
+                <p>Main content goes here.</p>
+                <Checkbox label='Pin drawer' checked={drawerPinned} onChange={toggleUiItem('DrawerPinned')} />
+                <Checkbox label='Show sidebar' checked={sidebarPinned} onChange={toggleUiItem('Sidebar')} />
+            </div>
+        </Panel>
+        <Sidebar pinned={sidebarPinned} width={5}>
+            <div><IconButton icon='close' onClick={toggleSidebar} /></div>
+            <div style={{flex: 1}}>
+                <p>Supplemental content goes here.</p>
+            </div>
+        </Sidebar>
+    </Layout>;
+};
+
+AppLayout.propTypes = {
+    drawerActive: PropTypes.bool.isRequired,
+    drawerPinned: PropTypes.bool.isRequired,
+    sidebarPinned: PropTypes.bool.isRequired,
+    toggleUiItem: PropTypes.func.isRequired,
+};
+
+export default AppLayout;
