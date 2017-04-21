edd2e3f1a8b3efd34452725cf2452e31668fbe27
Zetos Lima <cesar_manoel@hotmail.com>
2017-04-21 18:54:08 -0300

    SheetSections implemented

--- src/components/Sheet/SheetCard.js
+++ src/components/Sheet/SheetCard.js
@@ -1,44 +1,12 @@
 import React from 'react';
 import {Card, CardMedia, CardTitle, CardText, CardActions} from 'react-toolbox/lib/card';
-import Input from 'react-toolbox/lib/input';
+// import Input from 'react-toolbox/lib/input';
 
-import './SheetCard.css';
+// import styles from './SheetCard.css';
 import samanthaLips from '../../assets/samantha_lips02.jpg';
-// import SheetFields from './SheetFields.js';
+import SheetSections from './SheetSections.js';
 
 const dummyText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
-
-const FirstField = () => (
-      <section>
-        <Input type='text' label='Name' maxLength={50} />
-        <Input type='text' label='Player' />
-        <Input type='text' label='Chronicle' />
-      </section>
-);
-
-const SecondField = () => (
-      <section>
-        <Input type='text' label='Nature' />
-        <Input type='text' label='Demeanor' />
-        <Input type='text' multiline label='Concept' maxLength={90} />
-      </section>
-);
-
-const ThirdField = () => (
-      <section>
-        <Input type='text' label='Sire' maxLength={50} />
-        <Input type='text' label='Generation' />
-        <Input type='text' label='Haven' />
-      </section>
-);
-
-const FirstLine = () => (
-  <div>
-      <FirstField />
-      <SecondField />
-      <ThirdField />
-  </div>
-);
 
 const SheetCard = () => (
   <Card style={{width: '100%'}}>
@@ -54,13 +22,13 @@ const SheetCard = () => (
       title="Vampire the Masquerade - Character Sheet"
     />
     <CardText>
-      <FirstLine />
-      <FirstLine />
+      <SheetSections />
+      Second:
+      <SheetSections />
     </CardText>
     <CardActions>
-      <FirstField />
-      <SecondField />
-      <ThirdField />
+      CardActions
+      {dummyText}
     </CardActions>
   </Card>
 );
---
+++ src/components/Sheet/SheetSections.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Input from 'react-toolbox/lib/input';
+
+import styles from './SheetSections.css';
+
+const FirstSectionFieldOne = () => (
+      <div className={styles.fields}>
+        <Input type='text' label='Name' maxLength={50} />
+        <Input type='text' label='Player' />
+        <Input type='text' label='Chronicle' />
+      </div>
+);
+
+const FirstSectionFieldTwo = () => (
+      <div className={styles.fields}>
+        <Input type='text' label='Nature' />
+        <Input type='text' label='Demeanor' />
+        <Input type='text' label='Concept' maxLength={90} />
+      </div>
+);
+
+const FirstSectionFieldThree = () => (
+      <div className={styles.fields}>
+        <Input type='text' label='Sire' maxLength={50} />
+        <Input type='text' label='Generation' />
+        <Input type='text' label='Haven' />
+      </div>
+);
+
+const FirstSection = () => (
+  <section>
+      <FirstSectionFieldOne />
+      <FirstSectionFieldTwo />
+      <FirstSectionFieldThree />
+  </section>
+);
+
+const SheetSections = () => (
+    <div>
+      <FirstSection />
+    </div>
+);
+
+export default SheetSections;
