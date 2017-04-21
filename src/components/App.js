aa5c9b042f987bcc0166f84c0b8dce5259e7e1b0
Zetos Lima <cesar_manoel@hotmail.com>
2017-04-21 01:53:33 -0300

    Comments removed from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
-// import Footer from './Footer';
-// import AddTodo from '../containers/AddTodo';
-// import VisibleTodoList from '../containers/VisibleTodoList';
-// import PurpleAppBar from './PurpleAppBar.js';
 import ConnectedAppLayout from '../containers/ConnectedAppLayout';
 
-// const App = () => (
-//   <div>
-//     <PurpleAppBar />
-//     <AddTodo />
-//     <VisibleTodoList />
-//     <Footer />
-//   </div>
-// );
-
 const App = () => (
-  <ConnectedAppLayout />
+  <div>
+    <ConnectedAppLayout />
+  </div>
 );
 
 export default App;
