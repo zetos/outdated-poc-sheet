61c01f0d4dd5d5c276d3c84959f38a4244125078
Zetos Lima <cesar_manoel@hotmail.com>
2017-04-22 02:10:08 -0300

    SheetSections Component

--- src/components/Sheet/SheetSections.js
+++ src/components/Sheet/SheetSections.js
@@ -1,43 +1,112 @@
 import React from 'react';
-import Input from 'react-toolbox/lib/input';
 
-import styles from './SheetSections.css';
+import FirstSection from './FirstSection';
+import AttributesSection from './AttributesSection';
+import Abilities from './Abilities';
+import Advantages from './Advantages';
+import LastSection from './LastSection';
 
-const FirstSectionFieldOne = () => (
-      <div className={styles.fields}>
-        <Input type='text' label='Name' maxLength={50} />
-        <Input type='text' label='Player' />
-        <Input type='text' label='Chronicle' />
-      </div>
-);
+const handleSliderChange = (id, newValue) => {
+  console.log('handleSliderChange clicked');
+};
 
-const FirstSectionFieldTwo = () => (
-      <div className={styles.fields}>
-        <Input type='text' label='Nature' />
-        <Input type='text' label='Demeanor' />
-        <Input type='text' label='Concept' maxLength={90} />
-      </div>
-);
+const handleHealthChange = () => {
+  console.log('handleHealthChange clicked');
+};
 
-const FirstSectionFieldThree = () => (
-      <div className={styles.fields}>
-        <Input type='text' label='Sire' maxLength={50} />
-        <Input type='text' label='Generation' />
-        <Input type='text' label='Haven' />
-      </div>
-);
+// TODO handleFieldChange.
 
-const FirstSection = () => (
-  <section>
-      <FirstSectionFieldOne />
-      <FirstSectionFieldTwo />
-      <FirstSectionFieldThree />
-  </section>
-);
+// const LastSectionFieldOne = () => (
+//   <div className={styles.fields}>
+//     <h2>Merits/Flaws</h2> <br/>
+//     <Input type='text' label='merits/flaws' />
+//     <Input type='text' label='merits/flaws' />
+//     <Input type='text' label='merits/flaws' />
+//     <Input type='text' label='merits/flaws' />
+//     <Input type='text' label='merits/flaws' />
+//   </div>
+// );
+
+// const HumanityPathField = () => (
+//   <Slider pinned snaps min={0} max={10} step={1} editable value={0} onChange={handleSliderChange} />
+// );
+
+// const WillpowerField = () => (
+//   <div>
+//     <Slider disabled pinned snaps min={0} max={10} step={1} editable value={0} onChange={handleSliderChange} />
+//     <Slider pinned snaps min={0} max={10} step={1} editable value={0} onChange={handleSliderChange} />
+//   </div>
+// );
+
+// const BloodPollField = () => (
+//   <Slider pinned snaps min={0} max={20} step={1} editable value={0} onChange={handleSliderChange} />
+// );
+
+// const LastSectionFieldTwo = () => (
+//   <div className={styles.fields}>
+//     <h2>Humanity/Path</h2> <br />
+//     <HumanityPathField />
+//     <h2>Willpower</h2> <br />
+//     <WillpowerField />
+//     <h2>Blood Pool</h2> <br />
+//     <BloodPollField />
+//   </div>
+// );
+
+// const HealthField = () => {
+//   const handleHealthChange = () => {
+//     console.log('handleHealthChange clicked');
+//   };
+
+//   const healthLevels = [
+//     {value: 'Bruised', label: 'Bruised'},
+//     {value: 'Hurt', label: 'Hurt -1'},
+//     {value: 'Injured', label: 'Injured -1'},
+//     {value: 'Wounded', label: 'Wounded -2'},
+//     {value: 'Mauled', label: 'Mauled -2'},
+//     {value: 'Crippled', label: 'Crippled -5'},
+//     {value: 'Incapacitaed', label: 'Incapacitaed'},
+//   ];
+
+//   return (
+//   <Dropdown
+//     source={healthLevels}
+//     onChange={handleHealthChange}
+//     value={healthLevels[0].value}
+//   />
+//   );
+// };
+
+// const WeaknessField = () => (
+//   <Input type='text' label='weakness' />
+// );
+
+// const LastSectionFieldThree = () => (
+//   <div className={styles.fields}>
+//     <h2>Health</h2> <br />
+//     <HealthField />
+//     <h2>Weakness</h2> <br />
+//     <WeaknessField />
+//   </div>
+// );
+
+// const LastSection = () => (
+//   <div>
+//       <section>
+//         <LastSectionFieldOne />
+//         <LastSectionFieldTwo />
+//         <LastSectionFieldThree />
+//       </section>
+//   </div>
+// );
 
 const SheetSections = () => (
     <div>
       <FirstSection />
+      <AttributesSection handleSliderChange={handleSliderChange} />
+      <Abilities handleSliderChange={handleSliderChange} />
+      <Advantages handleSliderChange={handleSliderChange}/>
+      <LastSection handleSliderChange={handleSliderChange} handleHealthChange={handleHealthChange} />
     </div>
 );
 
