89b8f49c2073be126ed234c1759c3145d2b65f01
Zetos Lima <cesar_manoel@hotmail.com>
2017-04-22 01:09:51 -0300

    dummy text removed

--- src/components/Sheet/SheetCard.js
+++ src/components/Sheet/SheetCard.js
@@ -6,8 +6,6 @@ import {Card, CardMedia, CardTitle, CardText, CardActions} from 'react-toolbox/l
 import samanthaLips from '../../assets/samantha_lips02.jpg';
 import SheetSections from './SheetSections.js';
 
-const dummyText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
-
 const SheetCard = () => (
   <Card style={{width: '100%'}}>
     <CardTitle
@@ -26,7 +24,6 @@ const SheetCard = () => (
     </CardText>
     <CardActions>
       CardActions
-      {dummyText}
     </CardActions>
   </Card>
 );
